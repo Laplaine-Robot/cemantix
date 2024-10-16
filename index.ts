a9c838fa86687042f5ec81ebd6541e7b99d6165d
Jean-Brice <jeanbrice.danho@student-cs.fr>
2024-10-16 11:16:23 +0200

    La dernière partie de la deuxième section 2.1

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // If you have created a git repo
-import { serve } from "https://deno.land/std@0.119.0/http/server.ts>;
+import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
     if (_req.method ==
